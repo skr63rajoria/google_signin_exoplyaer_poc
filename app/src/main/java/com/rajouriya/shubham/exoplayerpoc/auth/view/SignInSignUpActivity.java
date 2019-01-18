f68377debfd8cd82e4b3c6c91f2bff148ef4ded3
itc_itd_team <itcnextstep@gmail.com>
2019-01-19 02:30:59 +0530

    first project set up commit

--- app/src/main/java/com/rajouriya/shubham/exoplayerpoc/auth/view/SignInSignUpActivity.java
+++ app/src/main/java/com/rajouriya/shubham/exoplayerpoc/auth/view/SignInSignUpActivity.java
@@ -1,9 +1,11 @@
-package com.rajouriya.shubham.exoplayerpoc;
+package com.rajouriya.shubham.exoplayerpoc.auth.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.rajouriya.shubham.exoplayerpoc.R;
+
+public class SignInSignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
